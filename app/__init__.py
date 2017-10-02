a95195249787c0069797f114b8baca0a7b331891
Trentham3269 <scottdalwood@gmail.com>
2017-10-02 23:21:25 +1100

    Add data models and login management

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,8 @@
 # Third part imports
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+from flask_migrate import Migrate
 
 # Local imports
 from config import app_config
@@ -8,15 +10,21 @@ from config import app_config
 # Database variable initialisation
 db = SQLAlchemy()
 
+# Login variable initialisation
+login_manager = LoginManager()
+
 def create_app(config_name):
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_object(app_config[config_name])
     app.config.from_pyfile('config.py')
     db.init_app(app)
-
-    # temproary route
-    @app.route('/')
-    def hello_world():
-        return 'Hello, World!'
+    
+    login_manager.init_app(app)
+    login_manager.login_message = "You must be logged in to access this page."
+    login_manager.login_view = "auth.login"
+    
+    migrate = Migrate(app, db)
+    
+    from app import models
 
     return app
